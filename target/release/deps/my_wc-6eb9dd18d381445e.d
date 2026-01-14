ac39b59546d70b7f796a544cc173cf595083d678
Mikkel Maalmand Jensen <info@MacBook-Pro-5.local>
2026-01-14 14:18:16 -0800

    wc done

---
+++ target/release/deps/my_wc-6eb9dd18d381445e.d
@@ -0,0 +1,5 @@
+/Users/info/code/371rs/01/target/release/deps/my_wc-6eb9dd18d381445e.d: src/main.rs
+
+/Users/info/code/371rs/01/target/release/deps/my_wc-6eb9dd18d381445e: src/main.rs
+
+src/main.rs:
---
+++ target/release/my_wc.d
@@ -0,0 +1,1 @@
+/Users/info/code/371rs/01/target/release/my_wc: /Users/info/code/371rs/01/src/main.rs
